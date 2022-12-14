1a668f404d0a4f45b343ecd5e2711b43687bb45c
Amit Bar-Gil <amit217@yandex.com>
2022-12-14 19:11:39 +0200

    chore: running sum

---
+++ rurnningSum/runningSum.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> runningSum(vector<int> &nums)
+    {
+        int sum = 0;
+        vector<int> result = {};
+        for (int i = 0; i < nums.size(); i++)
+        {
+            sum += nums[i];
+            result.push_back(sum);
+        };
+
+        return result;
+    }
+};
+
+int main()
+{
+    return 0;
+}
